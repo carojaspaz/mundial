a42a15e89afd4f8c4d15222fa15ee1aa28ca47c1
Carlos Andrés Rojas Paz <carojaspaz@gmail.com>
2023-08-25 02:32:46 -0500

    con fastapi

--- album/views.py
+++ album/views.py
@@ -4,7 +4,7 @@ from django.views.generic import CreateView, DeleteView, UpdateView, ListView, D
 from album.models import Selection, Player
 from .forms import PlayerCreateForm, PlayerUpdateForm
 
-# Create your views here.
+import requests
 
 
 class HomeView(TemplateView):
@@ -18,7 +18,38 @@ class SelectionListView(ListView):
 class SelectionCreate(CreateView):
     model = Selection
     fields = '__all__'
-    success_url = reverse_lazy('selection-list')
+    success_url = reverse_lazy('selection-list')    
+    
+    def form_valid(self, form):
+        # Guardar el objeto de formulario en memoria sin persistirlo aún
+        selection = form.save(commit=False)
+
+        # Obtener las rutas de las imágenes desde los campos del formulario
+        shield_path = form.cleaned_data['shield']
+        team_path = form.cleaned_data['team']
+
+        # Asignar las rutas de las imágenes al objeto de selección
+        selection.shield = shield_path
+        selection.team = team_path
+
+        # Guardar el objeto de selección con las rutas de las imágenes
+        selection.save()
+
+        # Consumir la API para crear la selección
+        api_url = "http://localhost:8080/selections/"  # Reemplaza con la URL correcta
+        data = {
+            "name": selection.name,
+            "shield": shield_path,
+            "team": team_path
+        }
+        response = requests.post(api_url, data=data)
+
+        if response.status_code == 201:
+            print("Selección creada en la API exitosamente.")
+        else:
+            print("Error al crear la selección en la API:", response.text)
+
+        return super().form_valid(form)
 
 
 class SelectionDetailView(DetailView):
